255af8b45e3dbc29592929dfbc169c0b47a3268c
Sipun_137 <subhranshu060@gmail.com>
2025-05-29 13:27:35 +0530

    feat: Configure the Context api for the Application for state management

---
+++ src/context/index.jsx
@@ -0,0 +1,36 @@
+"use client";
+import Cookies from "js-cookie";
+import {createContext, useEffect, useState} from "react";
+
+export const GlobalContext = createContext();
+
+export default function GlobalState({children}) {
+    const [isAuthUser, setAuthUser] = useState(null);
+    const [user, setUser] = useState(null);
+    const [role,setRole]=useState(null);
+    useEffect(() => {
+        if (Cookies.get("token") !== undefined) {
+            setAuthUser(true);
+            const userData = JSON.parse(localStorage.getItem("user"));
+            setRole(userData.role);
+            setUser(userData);
+        } else {
+            setAuthUser(false);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [Cookies]);
+
+    return (
+        <GlobalContext.Provider
+            value={{
+                isAuthUser,
+                setAuthUser,
+                user,
+                setUser,
+                role,setRole
+            }}
+        >
+            {children}
+        </GlobalContext.Provider>
+    );
+}
